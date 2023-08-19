576ff3f7ca7b8e3ddee59975ce9c4813d5f0c482
zachlamont <zachlamont1@gmail.com>
2023-08-19 12:17:48 +1000

    Add navigation bars

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,11 @@
+function RightSidebar() {
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3>Suggested Communities</h3>
+      </div>
+    </section>
+  );
+}
+
+export default RightSidebar;
