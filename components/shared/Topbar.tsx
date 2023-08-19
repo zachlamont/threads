e83cf5f886747796cd3fa90468bc24f88ee7204e
zachlamont <zachlamont1@gmail.com>
2023-08-19 14:08:36 +1000

    Update onboarding page

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,8 @@
 import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from '@clerk/themes'
+
 
 function Topbar() {
   return (
@@ -28,6 +30,7 @@ function Topbar() {
         </div>
         <OrganizationSwitcher
         appearance={{
+          baseTheme: dark,
           elements: {
             organizationSwitcherTrigger: "py-2 px-4"} 
           }} />
