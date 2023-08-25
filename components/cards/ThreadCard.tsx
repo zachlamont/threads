bf598eb6bc1817886e42d0dfedc0e7685ea9a214
zachlamont <zachlamont1@gmail.com>
2023-08-25 21:31:54 +1000

    Add profile header

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -39,7 +39,11 @@ const ThreadCard = ({
   isComment,
 }: Props) => {
   return (
-    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+    <article
+      className={`flex w-full flex-col rounded- ${
+        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
@@ -95,9 +99,9 @@ const ThreadCard = ({
               </div>
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
-                    <p className="mt-1 text-subtle-medium text-gray-1">
-                        {comments.length} replies
-                    </p>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
                 </Link>
               )}
             </div>
