a2a842393b54e17f033cca7652a1812be4948056
zachlamont <zachlamont1@gmail.com>
2023-08-26 12:13:38 +1000

    Implement ThreadsTab

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 import { profileTabs } from "@/constants";
+import ThreadsTab from "@/components/shared/ThreadsTab";
 
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
@@ -49,6 +50,19 @@ async function Page({ params }: { params: { id: string } }) {
               </TabsTrigger>
             ))}
           </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
         </Tabs>
       </div>
     </section>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { revalidatePath } from "@/node_modules/next/cache";
+import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
@@ -47,5 +48,33 @@ export async function fetchUser(userId: string) {
     //});
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string){
+
+  try {
+connectToDB();
+//Find all threads authored byuser with the given user Id 
+//TODO Populate Community
+
+const threads = await User.findOne({ id: userId })
+.populate({
+  path:'threads',
+  model: Thread,
+  populate: {
+    path: 'children',
+    model: Thread,
+    populate: {
+      path: 'author',
+      model: User,
+      select: 'name image id'
+    }
+  }
+})
+return threads;
+
+  } catch (error: any){
+throw new Error(`Failed to fetch user posts: ${error.message}`)
   }
 }
