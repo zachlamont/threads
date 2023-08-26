4e77f0dfbc0a9ea251b3d947c1eaeff11c07c0db
zachlamont <zachlamont1@gmail.com>
2023-08-26 16:22:32 +1000

    Implement activity page

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,23 @@
-const Page = async () => {
+
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
   return (
     <section>
       <h1 className="head-text mb-10">Communities</h1>
     </section>
   );
-};
+}
 
 export default Page;
